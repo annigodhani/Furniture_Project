93293d535bfa2e4c393379cc976204ee42f27b1a
annigodhani <aniruddhgodhani@gmail.com>
2024-09-11 16:09:44 +0530

    11/09/24

---
+++ controller/user/order.user.controller.js
@@ -0,0 +1,95 @@
+const OrderService = require('../../services/user/order.user.service');
+const CartService = require('../../services/user/cart.user.service');
+
+const orderService = new OrderService();
+const cartService = new CartService();
+
+
+
+exports.addNewOrder = async (req, res) => {
+    try {
+        const carts = await cartService.getallcart(req.query, req.user);
+
+        const orderItems = carts.map((item) => {
+            const quantity = item.quantity || 0;
+            const price = item.cartItem.price || 0;
+            return {
+                cartItem: item.cartItem._id,
+                quantity: quantity,
+                price: price
+            };
+        });
+
+        const totalAmount = orderItems.reduce((total, item) => {
+            const itemTotal = item.quantity * item.price;
+            return total + (isNaN(itemTotal) ? 0 : itemTotal);
+        }, 0);
+
+        if (isNaN(totalAmount)) {
+            throw new Error("Invalid totalAmount calculation");
+        }
+
+        const newOrder = {
+            user: req.user._id,
+            items: orderItems,
+            totalAmount: totalAmount
+        };
+
+        const order = await orderService.addNewOrder(newOrder);
+        await cartService.updatecart(req.user._id, { isDelete: true });
+
+        res.json({ order, message: 'Order Success....' });
+    }
+    catch (err) {
+        console.error(err);
+        res.json({ message: 'Internal Server Error' });
+    }
+};
+
+exports.getOrder = async (req, res) => {
+    try {
+        const order = await orderService.getOrder(req.body.orderID, { isDelete: false });
+
+        if (!order) {
+            return res.json("Order is Not found From this User..");
+        }
+
+        res.json(order);
+    }
+    catch (error) {
+        console.error(error);
+        res.json("Internal Server Error From GetAllOrder Controller");
+    }
+};
+
+exports.getAllOrder = async (req, res) => {
+    try {
+        const orders = await orderService.getAllOrder({ isDelete: false });
+
+        if (!orders) {
+            return res.json({ message: "Order is not found..." });
+        }
+
+        res.json(orders);
+    }
+    catch (error) {
+        console.error(error);
+        res.json({ message: "Internal Server Error From GetAllOrder Controller" });
+    }
+};
+
+exports.deleteOrder = async (req, res) => {
+    try {
+        const order = await orderService.deleteOrder(req.body.orderID, { isDelete: true });
+
+        if (!order) {
+            return res.json({ message: "Order is not found..." });
+        }
+
+        res.json({ order, message: "Order is Deleted Successfully" });
+    }
+    catch (error) {
+        console.error(error);
+        res.json({ message: "Internal Server Error From DeleteOrder Controller" });
+    }
+};
---
+++ server.js
@@ -0,0 +1,50 @@
+require('dotenv').config()
+const express = require('express')
+const server = express()
+const cors = require('cors')
+const path = require('path')
+const imagePath = path.join(__dirname, 'public', 'images')
+
+PORT = process.env.PORT
+dbURL = process.env.MONGO_URL
+
+const { default: mongoose } = require('mongoose')
+
+server.use(express.json())
+server.use(express.urlencoded({ extended: true }))
+server.use(cors())
+server.use('/public/images', express.static(imagePath))
+
+server.get('/', (req, res) => {
+   res.json({ message: 'welcome to express server' })
+})
+
+const userRoutes = require('./routes/userRoute/user.routes')
+const AdminRoutes = require('./routes/adminRoute/admin.routes')
+const productRoutes = require('./routes/adminRoute/product.admin.routes')
+const productuserRoutes = require('./routes/userRoute/product.user.routes')
+const foveriteroutes = require('./routes/userRoute/foverite.user.routes')
+const cartrouters = require('./routes/userRoute/cart.user.routes')
+const orderRoute = require('./routes/userRoute/order.user.routes')
+
+server.use('/api/user', userRoutes)
+server.use('/api/Admin', AdminRoutes)
+server.use('/api/product', productRoutes)
+server.use('/api/userproduct', productuserRoutes)
+server.use('/api/foveritecart', foveriteroutes)
+server.use('/api/cart', cartrouters)
+server.use('/api/order', orderRoute)
+
+
+
+
+server.listen(PORT, () => {
+   try {
+      mongoose.connect(dbURL)
+      console.log('mongodb start')
+   }
+   catch (error) {
+      handleError(error)
+   }
+   console.log('server is start at', 'http://localhost:1700')
+})
